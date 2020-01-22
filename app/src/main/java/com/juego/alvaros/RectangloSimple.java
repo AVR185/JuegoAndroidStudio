29f4a819a0301710d7cdf5dbe32f9dc51dcc230a
alvarorio97 <alvarorio97@gmail.com>
2020-01-22 16:30:02 +0100

    Clase base rectangulo y 2 subclases

---
+++ app/src/main/java/com/juego/alvaros/RectangloSimple.java
@@ -0,0 +1,13 @@
+package com.juego.alvaros;
+
+public class RectangloSimple extends Rectangulo {
+    /**Rectangulo que aparece en la parte superior de la pantalla en uan posicion aleatora en x
+     *  y se mueve hacia la parte inferior de la pantalla
+     *  a velocidad constante sin desplazarse en el eje x
+     */
+
+ public RectangloSimple(){
+     super();
+     setDireccion_x(0);
+ }
+}
